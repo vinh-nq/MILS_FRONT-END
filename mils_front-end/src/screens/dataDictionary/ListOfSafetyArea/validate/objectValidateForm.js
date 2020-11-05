880c4b0b01142380880870fe9d5e957c73f14fae
Nguyen Cuong <nguyencuong.3061997@gmail.com>
2020-11-05 15:17:01 +0700

    Update Safety Area vs Area Materal

---
+++ mils_front-end/src/screens/dataDictionary/ListOfSafetyArea/validate/objectValidateForm.js
@@ -0,0 +1,18 @@
+export const objectValidateForm = {
+  safetyAreaId : {
+    maxLength: 10,
+    checkEmpty: true,
+    nameLabel: "safetyAreaId",
+    checkDuplicate: true,
+  },
+  safetyAreaEng: {
+    maxLength: 40,
+    checkEmpty: true,
+    nameLabel: "safetyAreaEng",
+  },
+  safetyAreaLao: {
+    maxLength: 40,
+    checkEmpty: true,
+    nameLabel: "safetyAreaLao",
+  },
+};
